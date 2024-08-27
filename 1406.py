ea438813999b390b90110d6f78b4649b06219d17
hijyunk <ziggzagg@ajou.ac.kr>
2024-08-28 08:24:51 +0900

    시간 초과

--- 1406.py
+++ 1406.py
@@ -1,1 +1,27 @@
+import sys
+input = sys.stdin.readline
 
+char = input()
+m = int(input())
+
+commands = []
+for _ in range(m):
+    commands.append(input())
+    
+loc = len(char)
+char_list = list(char)
+
+for command in commands:
+    if command[0] == 'L':
+        if loc > 0: loc -= 1
+    elif command[0] == 'D':
+        if loc < len(char): loc += 1
+    elif command[0] == 'B':
+        if loc > 0:
+            loc -= 1
+            del char_list[loc]
+    elif command[0] == 'P':
+        char_list.insert(loc, command.split()[1])
+        loc += 1
+        
+print(*char_list, sep="")
