73b520bd8b52f3356103a6306bb2e3f7aac6e64a
Hyeoun_Ju <wjdguswn0954@gmail.com>
2024-08-28 00:13:35 +0900

    시간 초과 해결을 위해 양쪽에 스택 두기

--- 1406.py
+++ 1406.py
@@ -1,36 +1,27 @@
 import sys
 
 # 문장 입력 받기
-stream = list(sys.stdin.readline())
+stream = sys.stdin.readline()
 
-# 최대 글자
-if len(stream) >= 100000:
-    stream = stream[:100000]
-# print(stream)
+# 양쪽에 커서 두기
+left_stack = list(stream) # 왼쪽 스택에는 stream을 list 형태로 담기
+right_stack = [] # 빈 스택
 
-# 위치를 알려줄 커서
-cursor = len(stream)+1
-
-# 명령어 입력
-for _ in range(int(input())):
-    c = input().split(' ')
-    # print(cursor)
-    if c[0] == 'L':
-        if cursor > 0:
-            cursor -= 1
+# 명령어 처리
+for _ in range(int(sys.stdin.readline())):
+    c = sys.stdin.readline().split(' ')
+    if c[0] == 'L': # 커서를 왼쪽으로 한 칸
+        if left_stack:
+            right_stack.append(left_stack.pop())
     if c[0] == 'D':
-        if cursor < len(stream):
-            cursor += 1
+        if right_stack:
+            left_stack.append(right_stack.pop())
     if c[0] == 'B':
-        if cursor > 0:
-            if cursor > len(stream):
-                del stream[cursor-2]
-            else:
-                del stream[cursor-1]
-            cursor -= 1
+        if left_stack:
+            left_stack.pop()
     if c[0] == 'P':
-        stream.insert(cursor, c[1])
+        left_stack.append(c[1])
 
-
-print(''.join(stream))
+# 결과 출력 (커서 왼쪽 + 오른쪽 스택의 역순)
+print(''.join(left_stack + right_stack[::-1]))
         
