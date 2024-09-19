316838db9f00c9cb7a37c3453211fb0e685d3b2c
Sy3058 <apo1803058@gmail.com>
2024-09-19 22:57:01 +0900

    10799 at2

--- week2/10799.py
+++ week2/10799.py
@@ -1,35 +1,17 @@
 arr = input()
-stack_le = [] # 왼쪽 괄호
-stack_ri = [] # 오른쪽 괄호
-stack_l = [] # 레이저
-answer = 0
+arr_ch = arr.replace('()', '+')  # 레이저를 '+'로 변환
 
-arr_ch = arr.replace('()', '+') # 변형된 array
+answer = 0  # 최종 결과값 (잘린 쇠막대기 조각 수)
+stack = []  # 열린 괄호 '('를 저장할 스택
 
-for i in range (len(arr_ch)):
-  if arr_ch[i] == '(':
-    stack_le.append(i)
-  elif arr_ch[i] == ')':
-    stack_ri.append(i)
-  else:
-    stack_l.append(i)
-
-while stack_ri: # 왼쪽 스택이 하나가 남을 때까지 반복
-  temp_r = stack_ri.pop(0)
-  count = 0 # 레이저가 쇠막대기를 자른 횟수
-  for i in range (len(stack_le)):
-    if temp_r < stack_le[i]: # 이때 괄호가 완성됨
-      for j in stack_l:
-        if stack_le[i-1] < j < temp_r: # 쇠막대기를 자르는 레이저가 있음
-          count += 1
-      answer += count + 1 # 쇠막대기가 잘린 갯수 = 레이저 개수 + 1
-      stack_le.pop(i-1)
-      break
-
-count = 0
-for i in stack_l:
-  if stack_le[0] < i < temp_r: # 마지막 괄호
-    count += 1
-answer += count + 1
+# 배열을 순회하면서 괄호와 레이저 처리
+for i in range(len(arr_ch)):
+    if arr_ch[i] == '(':  # 열린 괄호: 쇠막대기 시작
+        stack.append('(')  # 스택에 추가
+    elif arr_ch[i] == '+':  # 레이저: 현재 스택에 있는 열린 괄호만큼 쇠막대기가 잘림
+        answer += len(stack)  # 스택의 길이 = 열린 괄호의 수 = 잘리는 쇠막대기의 수
+    elif arr_ch[i] == ')':  # 닫힌 괄호: 쇠막대기 끝남
+        stack.pop()  # 열린 괄호 하나 닫힘
+        answer += 1  # 쇠막대기의 끝이므로 잘린 조각 하나 추가
 
 print(answer)
