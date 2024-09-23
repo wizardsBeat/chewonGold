f22f080457cb7485aa852e957469cba1b58c3225
jijijiwon <ljiwon0310@gmail.com>
2024-09-23 11:31:43 +0900

    2293

---
+++ week3/2293.py
@@ -0,0 +1,17 @@
+import sys
+
+n, k = map(int, input().split()) 
+coins = []
+num = 0
+
+for i in range(n):
+    coins.append(int(sys.stdin.readline()))
+
+dp = [0] * (k + 1)
+dp[0] = 1
+
+for coin in coins:
+    for i in range(coin, k + 1):
+        dp[i] += dp[i - coin]
+
+print(dp[k])
