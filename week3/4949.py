1646b87d3303365b3913f8c01f1d80a7d0be0dbd
Sy3058 <apo1803058@gmail.com>
2024-09-24 01:11:41 +0900

    4949 att5

--- week3/4949.py
+++ week3/4949.py
@@ -4,21 +4,26 @@ sen = sys.stdin.readline().rstrip()
 
 while sen != '.':
   stack = []
+  balance = True # 괄호의 균형이 맞는 상태인지 확인
   for i in range (len(sen)):
     if sen[i] == '(':
       stack.append(sen[i])
     elif sen[i] == '[':
       stack.append(sen[i])
     elif sen[i] == ')':
-      if stack:
-        if stack[-1] == '(':
-          stack.pop()
+      if stack and stack[-1] == '(':
+        stack.pop()
+      else:
+        balance = False
+        break # )만 나오는 경우 뒤에 뭐가 나와도 balance가 맞지 않음
     elif sen[i] == ']':
-      if stack:
-        if stack[-1] == '[':
-          stack.pop()
-  if stack:
-    print('no')
-  else:
+      if stack and stack[-1] == '[':
+        stack.pop()
+      else:
+        balance = False
+        break
+  if balance and not stack:
     print('yes')
+  else:
+    print('no')
   sen = sys.stdin.readline().rstrip()
