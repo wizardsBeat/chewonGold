30c167974ddd127a7f1fd884784fbbddff13fb3d
Sy3058 <apo1803058@gmail.com>
2024-10-26 01:25:56 +0900

    19637 bisect

--- week7/19637.py
+++ week7/19637.py
@@ -1,41 +1,19 @@
 import sys
+import bisect
 
 N, M = map(int, sys.stdin.readline().split())
-options = {} # key가 칭호 이름
-options_power = {} # key가 power
+options = [] # 칭호
+powers = [] # 기준이 되는 값
 
 for _ in range (N):
-  option, value = sys.stdin.readline().split()
-  value = int(value)
-  if value not in options.values(): # 앞에 동일한 조건이 있었다면 뒤의 조건은 처리되지 않으므로 조건에 넣지 않아도 됨
-    options[option] = value
+  option, power = sys.stdin.readline().split()
+  power = int(power)
+  if len(powers) == 0 or power > powers[-1]: # power == powers[-1]인 경우에는 추가하지 않음
+    options.append(option)
+    powers.append(power)
 
-for key, value in options.items():
-  options_power[value] = key # key와 value 값을 반대로 저장
-
-p_op = list(options.values()) # 조건에 해당하는 값을 저장
-
+# bisect_left(array, x): 정렬된 순서를 유지하면서 array에 데이터 x를 삽입할 가장 왼쪽 인덱스를 찾음
 for _ in range (M):
-  power = int(sys.stdin.readline())
-
-  left = 0
-  right = len(p_op)
-  
-  while True:
-    mid = (left+right)//2
-    if power > p_op[mid]:
-      if power <= p_op[mid+1]: # 조건 만족 (해당하는 칭호가 없는 전투력은 주어지지 않으므로 mid+1이 range를 벗어날 수 없음)
-        print(options_power[p_op[mid+1]])
-        break
-      else: # left의 값이 더 커져야함
-        left = mid
-    else: # power <= p_op[mid]
-      if mid == 0:
-        print(options_power[p_op[0]])
-        break
-      else:
-        if p_op[mid-1] < power: # 조건 만족
-          print(options_power[p_op[mid]])
-          break
-        else:
-          right = mid # right의 값이 더 작아져야함
+  p = int(sys.stdin.readline())
+  idx = bisect.bisect_left(powers, p)
+  print(options[idx])
