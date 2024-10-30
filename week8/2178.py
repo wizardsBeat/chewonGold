5a2cf1cbb79ee4b358767915676431aa3fb4f439
osy0602 <osy01036095318@gmail.com>
2024-10-30 10:07:50 +0900

    2178문제 풀이

---
+++ week8/2178.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+maze = []
+for _ in range(N):
+    maze.append(list(map(int, input().strip())))
+
+def BFS(x, y):
+    dx = [-1, 1, 0,0]
+    dy = [0,0,-1,1]
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx<0 or nx>=N or ny<0 or ny>=M:
+                continue
+            if maze[nx][ny]==0:
+                continue
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y]+1
+                queue.append((nx,ny))
+                
+    
+    return maze[N-1][M-1]
+
+print(BFS(0,0))
