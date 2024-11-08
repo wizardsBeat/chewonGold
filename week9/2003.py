8be3327ebf54e09d0ac1cbc5a324fe5c502d979e
Sy3058 <apo1803058@gmail.com>
2024-11-08 19:42:15 +0900

    2003 two pointer

--- week9/2003.py
+++ week9/2003.py
@@ -5,19 +5,26 @@ arr = list(map(int, sys.stdin.readline().split()))
 i = 0 # left pointer
 j = 0 # right pointer
 cnt = 0
+hap = arr[0] # i == 0, j == 0 일 때의 합
 
 while j < N:
-  tmp = sum(arr[i:j+1])
-
-  if tmp == M:
+  if hap == M:
     cnt += 1
     i += 1
     j += 1
+    if j == N:
+      break
+    hap -= arr[i-1]
+    hap += arr[j]
   
-  elif tmp < M: # 합을 늘려야하므로 j를 늘려야 함
+  elif hap < M: # 합을 늘려야하므로 j를 늘려야 함
     j += 1
+    if j == N:
+      break
+    hap += arr[j]
   
   else: # 합을 줄여야하므로 i를 늘려야 함
     i += 1
+    hap -= arr[i-1]
 
 print(cnt)
