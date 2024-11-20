0443cfe753650008ef7984e62547695a00c8f3bd
Sy3058 <apo1803058@gmail.com>
2024-11-20 14:20:53 +0900

    12789 merge

--- week10/12789.py
+++ week10/12789.py
@@ -1,23 +1,32 @@
-import sys
-from collections import deque
+def dokidoki_snack_delivery():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    n = int(data[0])
+    students = list(map(int, data[1:]))
+    
+    stack = []
+    current = 1  # 간식을 나눠줄 순번
+    
+    for student in students:
+        while stack and stack[-1] == current:
+            stack.pop()
+            current += 1
+        if student == current:
+            current += 1
+        else:
+            stack.append(student)
+    
+    # 남아있는 스택을 확인
+    while stack:
+        if stack.pop() == current:
+            current += 1
+        else:
+            print("Sad")
+            return
+    
+    print("Nice")
 
-N = int(sys.stdin.readline())
-arr = deque((map(int, sys.stdin.readline().split())))
-side = deque([])
-flag = True
 
-for i in range (1, N+1):
-  if i in arr:
-    while arr[0] != i:
-      tmp = arr.popleft()
-      side.append(tmp)
-    arr.popleft() # i 제거
-  
-  elif side[-1] == i:
-    side.pop()
-  
-  else: # 공간에 있는 사람은 대기열로 돌아갈 수 없으므로 i가 arr에 없고 side[-1]도 아닌 경우 처리 불가
-    flag = False
-    break
-
-print("Nice" if flag else "Sad")
+dokidoki_snack_delivery()
