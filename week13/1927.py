b4b7505c5074b7ed1e1618d1a74428871fa7f54f
Sy3058 <apo1803058@gmail.com>
2024-12-05 12:57:44 +0900

    1927 heap

---
+++ week13/1927.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+heap = []
+
+for _ in range (n):
+  x = int(sys.stdin.readline())
+
+  if not heap and x == 0:
+    print(0)
+  
+  elif x == 0:
+    print(heapq.heappop(heap))
+  
+  else:
+    heapq.heappush(heap, x)
