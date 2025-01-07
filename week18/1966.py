389387711da8b9f685763af72bb05f13fb521684
Sy3058 <apo1803058@gmail.com>
2025-01-08 03:20:32 +0900

    1966 deque

--- week18/1966.py
+++ week18/1966.py
@@ -2,25 +2,22 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-t = int(input())
-for _ in range (t):
-  n, m = map(int, input().split())
-  imp = list(map(int, input().split()))
-  doc = deque(list(enumerate(imp)))
-  flag = False
-  
-  imp.sort(reverse=True)
+def printer():
   for i in range (n):
     for j in range (len(doc)):
       if doc[j][1] == imp[i]:
         if doc[j][0] == m:
           ans = i + 1
-          flag = True
-          break
+          return ans
         doc.rotate(-j)
         doc.popleft()
         break
-    if flag:
-      break
-  
+
+t = int(input())
+for _ in range (t):
+  n, m = map(int, input().split())
+  imp = list(map(int, input().split()))
+  doc = deque(list(enumerate(imp)))
+  imp.sort(reverse=True)
+  ans = printer()
   print(ans)
