25ea34a45be93ff6a76aa0f3cacd16b005f735a3
Sy3058 <apo1803058@gmail.com>
2025-02-11 18:24:58 +0900

    1991 recursion

--- week22/1991.py
+++ week22/1991.py
@@ -6,49 +6,26 @@ tree = {}
 
 for _ in range (n):
   c, l, r = input().strip().split()
-  tree[c] = [l, r]
-
-rpre, rio, rpost = [], [], []
+  tree[c] = (l, r)
 
 def pre(node):
-  if node not in rpre:
-    rpre.append(node)
-  if tree[node][0] != '.':
-    rpre.append(tree[node][0])
-    pre(tree[node][0])
-  if tree[node][1] != '.':
-    rpre.append(tree[node][1])
-    pre(tree[node][1])
-  return rpre
-
-print(''.join(pre('A')))
+  if node == '.':
+    return ""
+  l, r = tree[node]
+  return node + pre(l) + pre(r)
 
 def io(node):
-  if tree[node][0] != '.':
-    io(tree[node][0])
-    if tree[node][0] not in rio:
-      rio.append(tree[node][0])
-  if node not in rio:
-    rio.append(node)
-  if tree[node][1] != '.':
-    io(tree[node][1])
-    if tree[node][1] not in rio:
-      rio.append(tree[node][1])
-  return rio
-  
-print(''.join(io('A')))
+  if node == '.':
+    return ""
+  l, r = tree[node]
+  return io(l) + node + io(r)
 
 def post(node):
-  if tree[node][0] != '.':
-    post(tree[node][0])
-    if tree[node][0] not in rpost:
-      rpost.append(tree[node][0])
-  if tree[node][1] != '.':
-    post(tree[node][1])
-    if tree[node][1] not in rpost:
-      rpost.append(tree[node][1])
-  if node not in rpost:
-    rpost.append(node)
-  return rpost
+  if node == '.':
+    return ""
+  l, r = tree[node]
+  return post(l) + post(r) + node
 
-print(''.join(post('A')))
+print(pre('A'))
+print(io('A'))
+print(post('A'))
