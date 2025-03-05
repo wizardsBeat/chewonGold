cc1dc800ed2724081a488ac6757fb7e34e5a9ea7
wjdguswn1203@naver.com <84135721+wjdguswn1203@users.noreply.github.com>
2025-03-05 10:10:11 +0900

    투 포인터로 해결

--- week24/2470.py
+++ week24/2470.py
@@ -2,25 +2,31 @@ import sys
 
 input = sys.stdin.readline
 
+# 입력 받기
 N = int(input())
 M = list(map(int, input().split(' ')))
-min = 1000000000
+
+M.sort()
+
+# 최대값 설정
+min = float('inf')
+
+# 0과 가장 가까운 조합 => return 값
 min_list = [0,0]
 
-for n in range(N):
-    a = M[n]
-    for m in range(N):
-        if m == 0 or m == N:
-            pass
-        else:
-            b = M[m]
-            sum = a+b
-            if sum < 0:
-                sum *= -1
-            if min > sum:
-                min = sum
-                min_list[0] = M[n]
-                min_list[1] = M[m]
+# 투 포인터
+lp, rp = 0, N-1
+
+while(lp < rp):
+    sum = M[lp] + M[rp]
+    if abs(sum) < abs(min):
+        min = sum
+        min_list = [M[lp], M[rp]]
+    
+    if sum > 0:
+        rp -= 1
+    else:
+        lp += 1
     
 print(str(min_list[0])+" "+str(min_list[1]))
 
