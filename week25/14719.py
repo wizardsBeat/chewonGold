f0c3dbfebc6584a959cfc4d75267a4415df49374
Sy3058 <apo1803058@gmail.com>
2025-03-06 17:14:58 +0900

    14719 sim

--- week25/14719.py
+++ week25/14719.py
@@ -2,18 +2,15 @@ import sys
 input = sys.stdin.readline
 
 h, w = map(int, input().split())
-height = list(map(int, input().split()))
+wall = list(map(int, input().split()))
 
-wall = {i:[] for i in range (h)}
-water = 0
+ans = 0
+for i in range (1, w-1): # 어차피 양 끝은 블럭이 있든 없든 물이 찰 수 없음
+  l = max(wall[:i]) # 현재 위치를 기준으로 왼쪽에 있는 블럭 중 높이가 가장 높은 블럭
+  r = max(wall[i+1:]) # 오른쪽에 있는 블럭 중 높이가 가장 높은 블럭
+  rain = min(l, r) # 둘 중 낮은 블럭의 높이만큼 빗물이 찰 수 있음
 
-for i in range (w):
-  for j in range (height[i]):
-    wall[j].append(i)
+  if rain > wall[i]: # 현재 위치의 높이보다 빗물의 높이가 높아야 빗물이 고임
+    ans += rain - wall[i]
 
-for i in range (h):
-  col = wall[i]
-  for j in range (1, len(col)):
-    water += col[j] - col[j-1] - 1
-
-print(water)
+print(ans)
