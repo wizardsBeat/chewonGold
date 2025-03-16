3cfe64f2a086b1960368f8c0d0d8eb971127bb7f
Hyeoun_Ju <wjdguswn0954@gmail.com>
2025-03-16 23:51:05 +0900

    dp 사용해서 문제 풀기 완료

--- week26/2156.py
+++ week26/2156.py
@@ -1,38 +1,29 @@
+# 03.16 PM 9시 테스트 케이스는 풀었지만 반례에 오류가 생김
+# 03.16 PM 11시 강의를 찾아보니 다이나믹 프로그래밍을 사용하라고 함
+# 03.16 PM 11:40 max 오류 해결
+import sys
 # 연속 3잔 불가능, 최대 섭취량
 def wine_select(n, wines):
-    wine_drink = 0
-    # 문제 2: 입력이 하나일 대 None이 됨, 해결
-    current_max = max(wines)
-    current_point = 0
-    while(current_point < n//2):
-        # 문제 1: 입력이 두 개일 때 None이 됨, 해결
-        if n < 3:
-            current_max = wines[0] + wines[1]
-            break
-        for i in range(n):
-            point = current_point + i
-            if (point + 1) % 3 == 0:
-                pass
-            else:
-                wine_drink += wines[point]
-                print(f'바뀐값: {wine_drink}')
-        if wine_drink > current_max:
-            current_max = wine_drink
-        wine_drink = 0
-        current_point += 1
-    return current_max
-            
+    dp = [0 for _ in range(10001)]
+    dp[1] = wines[1]
+    dp[2] = wines[1] + wines[2]
+    dp[3] = max(wines[1] + wines[3], max(wines[2]+wines[3], dp[2]))
+    for i in range(4, n+1):
+        dp[i] = max(wines[i]+wines[i-1]+dp[i-3],max(wines[i]+dp[i-2],dp[i-1]))
+    return dp[n]
+    
+
 
 # 포두주 잔의 개수
+input = sys.stdin.readline
 n = int(input())
 
 # 포도주 리스트
-wines = []
-for _ in range(n):
-    wines.append(int(input()))
-print(f'포도주들: {wines}')
+wines = [0 for _ in range(10001)]
+for i in range(1,n+1):
+    wines[i] = int(input())
 
 # 포도주 최대 섭취량
-print(f'최대 섭취량: {wine_select(n, wines)}')
+print(wine_select(n, wines))
 
 
